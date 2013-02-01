2f59f0b412adc4efcf331d4696a2248f946c9a86
Frederick Giasson <fred@fgiasson.com>
2013-02-01 10:18:13 -0500

    Add two new features:
      (1) loading/importing a single ontology file
      (2) listing all loaded ontologies

---
+++ inc/getLoadedOntologies.php
@@ -0,0 +1,77 @@
+<?php
+
+  use \StructuredDynamics\structwsf\php\api\ws\ontology\read\OntologyReadQuery;
+  use \StructuredDynamics\structwsf\php\api\ws\ontology\read\GetLoadedOntologiesFunction;
+
+  function getLoadedOntologies($structwsf)
+  {
+    $ontologyRead = new OntologyReadQuery($structwsf);
+    
+    $getLoadedOntologiesFunction = new GetLoadedOntologiesFunction();
+    $getLoadedOntologiesFunction->modeDescriptions();
+    
+    $ontologyRead->getLoadedOntologies($getLoadedOntologiesFunction)
+                 ->send();
+        
+    if($ontologyRead->isSuccessful())
+    {
+      $resultset = $ontologyRead->getResultset()->getResultset();
+      
+      $ontologies = array(
+        'local' => array(),
+        'reference' => array(),
+        'admin' => array()
+      );
+
+      foreach($resultset['unspecified'] as $uri => $ontology)
+      {
+        $onto = array(
+          'uri' => '',
+          'label' => '',
+          'modified' => false
+        );        
+
+        $ontologyType = 'local';
+        
+        if(isset($ontology['http://purl.org/ontology/sco#ontologyType']))
+        {
+          switch($ontology['http://purl.org/ontology/sco#ontologyType'][0]['uri'])
+          {
+            case "http://purl.org/ontology/sco#referenceOntology":
+              $ontologyType = 'reference';
+            break;
+            case "http://purl.org/ontology/sco#administrativeOntology":
+              $ontologyType = 'admin';
+            break;
+            case "http://purl.org/ontology/sco#localOntology":
+              $ontologyType = 'local';
+            break;
+          }
+        }
+        
+        $onto['uri'] = $uri;
+        $onto['label'] = $ontology['prefLabel'];
+        
+        if(isset($ontology['http://purl.org/ontology/wsf#ontologyModified']))
+        {
+          $onto['modified'] = TRUE;
+        }
+
+        array_push($ontologies[$ontologyType], $onto);        
+      }
+      
+      return($ontologies);
+    }
+    else
+    {
+      $debugFile = md5(microtime()).'.error';
+      file_put_contents('/tmp/'.$debugFile, var_export($ontologyRead, TRUE));
+     
+      @cecho('Can\'t get loaded ontologies. '. $ontologyRead->getStatusMessage() . 
+             $ontologyRead->getStatusMessageDescription()."\nDebug file: /tmp/$debugFile\n", 'RED');
+             
+      exit;
+    }    
+  }
+  
+?>
--- sync.php
+++ sync.php
@@ -21,23 +21,30 @@
   }  
   
   // Get commandline options
-  $arguments = getopt('h::l:', array('help::',
+  $arguments = getopt('h::l::', array('help::',
+                                      'load::',
+                                      'list::',
                                       'load-all::',
                                       'load-list::',
                                       'load-advanced-index::',
                                       'load-structwsf::',
-                                      'load-force-reload::'));  
+                                      'load-force-reload::',
+                                      'list-structwsf::'));  
   
   // Displaying DSF's help screen if required
   if(isset($arguments['h']) || isset($arguments['help']))
   {
     cecho("Usage: php sync.php [OPTIONS]\n\n\n", 'WHITE');
     cecho("Usage examples: \n", 'WHITE');
-    cecho("    Load all ontologies: php sync.php --load-all --load-list=\"/data/ontologies/sync/ontologies.lst --load-structwsf=http://localhost/ws/\"\n", 'WHITE');
+    cecho("    Load all ontologies: php sync.php --load-all --load-list=\"/data/ontologies/sync/ontologies.lst\" --load-structwsf=\"http://localhost/ws/\"\n", 'WHITE');
+    cecho("    Load one ontology: php sync.php --load=\"http://purl.org/ontology/bibo/\" --load-structwsf=\"http://localhost/ws/\"\n", 'WHITE');
+    cecho("    List loaded ontologies: php sync.php --list --list-structwsf=http://localhost/ws/\"\n", 'WHITE');
     cecho("\n\n\nOptions:\n", 'WHITE');
     cecho("-l, --load-all                          Load all the ontologies from a list of URLs\n\n", 'WHITE');
+    cecho("--load=\"[URL]\"                          Load a single ontology\n\n", 'WHITE');
+    cecho("--list                                  List all loaded ontologies\n\n", 'WHITE');
     cecho("-h, --help                              Show this help section\n\n", 'WHITE');
-    cecho("Load All Options:\n", 'WHITE');
+    cecho("Load Options:\n", 'WHITE');
     cecho("--load-list=\"[FILE]\"                    (required) File path where the list can be read.\n", 'WHITE');
     cecho("                                                   The list is a series of space-seperated\n", 'WHITE');
     cecho("                                                   URLs where ontologies files are accessible\n", 'WHITE');
@@ -54,6 +61,9 @@
     cecho("                                                   Example: 'http://localhost/ws/'\n", 'WHITE');
     cecho("--load-force-reload=\"[BOOL]\"            (optional) Default is false. If true, it means all the ontologies\n", 'WHITE');
     cecho("                                                   will be deleted and reloaded/re-indexed in structWSF\n", 'WHITE');
+    cecho("List Options:\n", 'WHITE');
+    cecho("--list-structwsf=\"[URL]\"                (required) Target structWSF network endpoints URL.\n", 'WHITE');
+    cecho("                                                   Example: 'http://localhost/ws/'\n", 'WHITE');
     
     exit;
   }
@@ -65,12 +75,58 @@
   $structwsfFolder = rtrim($setup["config"]["structwsfFolder"], "/");
   
   include_once($structwsfFolder."/StructuredDynamics/SplClassLoader.php");   
-
-  // Reload all ontologies
-  if(isset($arguments['l']) || isset($arguments['load-all']))
+ 
+  // List loaded ontologies
+  if(isset($arguments['list']))
   {
     // Make sure the required arguments are defined in the arguments
-    if(empty($arguments['load-list']))
+    if(empty($arguments['list-structwsf']))
+    {
+      cecho("Missing the --list-structwsf parameter for listing ontologies.\n", 'RED');  
+      
+      exit;
+    }    
+    
+    include_once('inc/getLoadedOntologies.php');
+    
+    $ontologies = getLoadedOntologies($arguments['list-structwsf']);
+    
+    $nb = 0;
+    
+    cecho("Local Ontologies: \n", 'WHITE');
+    
+    foreach($ontologies['local'] as $ontology)
+    {
+      $nb++;
+      
+      cecho("  ($nb) ".$ontology['label'].($ontology['modified'] ? '  '.cecho('[modified; not saved]', 'YELLOW', TRUE) : '')."\n", 'WHITE');
+    }
+    
+    cecho("\nReference Ontologies: \n", 'WHITE');
+    
+    foreach($ontologies['reference'] as $ontology)
+    {
+      $nb++;
+      
+      cecho("  ($nb) ".$ontology['label'].($ontology['modified'] ? '  '.cecho('[modified; not saved]', 'YELLOW', TRUE) : '')."\n", 'WHITE');
+    }
+    
+    cecho("\nAdministrative Ontologies: \n", 'WHITE');
+    
+    foreach($ontologies['admin'] as $ontology)
+    {
+      $nb++;
+      
+      cecho("  ($nb) ".$ontology['label'].($ontology['modified'] ? '  '.cecho('[modified; not saved]', 'YELLOW', TRUE) : '')."\n", 'WHITE');
+    }
+  }
+  
+  // Reload all ontologies
+  if(isset($arguments['l']) || isset($arguments['load-all']) || isset($arguments['load']))
+  {
+    DebugBreak();
+    // Make sure the required arguments are defined in the arguments
+    if(empty($arguments['load-list']) && !isset($arguments['load']))
     {
       cecho("Missing the --load-list parameter for loading all the ontologies.\n", 'RED');  
       
@@ -86,14 +142,24 @@
     }
     
     // Load all the ontologies from the input list
-    if(!file_exists($arguments['load-list']))
+    if(!file_exists($arguments['load-list']) && !isset($arguments['load']))
     {
       cecho("Input file of --load-list is not exising on this system.\n", 'RED');  
       
       exit;
     }
     
-    $ontologiesUrls = explode(' ', file_get_contents($arguments['load-list']));
+    $ontologiesUrls = array();
+    
+    if(!isset($arguments['load']))
+    {
+      $ontologiesUrls = explode(' ', file_get_contents($arguments['load-list']));
+    }
+    else
+    {
+      // There is only a single ontology URL to load for this command
+      array_push($ontologiesUrls, $arguments['load'])  ;
+    }
     
     foreach($ontologiesUrls as $url)
     {
